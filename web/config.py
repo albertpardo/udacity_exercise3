50b1a6d54ac5624e1b7b1fac41129b56448602b7
APM <albertopardomarti@gmail.com>
2021-10-27 16:46:40 +0200

    prueba: Web local y DB en azure

--- web/config.py
+++ web/config.py
@@ -6,19 +6,19 @@ class BaseConfig:
     DEBUG = True
 
     # Local Host URL
-    HOST = "localhost"
+    HOST = "migrate-app-server.postgres.database.azure.com"
     PORT = 5432
     POSTGRES_URL='{}:{}'.format(HOST, PORT)
-    POSTGRES_USER="root" #TODO: Update value
-    POSTGRES_PW="root"   #TODO: Update value
+    POSTGRES_USER="azureuser@migrate-app-server" #TODO: Update value
+    POSTGRES_PW="myPassword1"   #TODO: Update value
     POSTGRES_DB="techconfdb"   #TODO: Update value
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
     SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
     
-    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://migrate-app-bus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=NwAP2ykDl9AJkkudcSB/4jnafayABJDy1metMWjM760=' #TODO: Update value
-    SERVICE_BUS_QUEUE_NAME ='migratequeue'
+    SERVICE_BUS_CONNECTION_STRING ='' #TODO: Update value
+    SERVICE_BUS_QUEUE_NAME ='notificationqueue'
     
     ADMIN_EMAIL_ADDRESS = 'albertopardomarti@gmail.com'
     SENDGRID_API_KEY = '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' #Configuration not required, required SendGrid Account
